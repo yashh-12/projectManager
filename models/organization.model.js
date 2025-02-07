04b6e10a99288dd7b753cc467f1dbea1ced7f62b
Yash <yashpadhiyar.pyr@gmail.com>
2025-02-07 17:44:32 +0530

    Add organization model

---
+++ models/organization.model.js
@@ -0,0 +1,31 @@
+// _id string pk
+// name string
+// owner string
+// projects string[]
+
+import mongoose, {Schema} from "mongoose"
+
+const organizationSchema = new Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            refs: "User"
+        },
+        projects: [
+            {
+                type: Schema.Types.ObjectId,
+                refs: "Project"
+            }
+        ]
+    },
+    {timestamp: true}
+)
+
+const Organization = mongoose.model("Organization", organizationSchema)
+
+export default Organization
